a0d63714f382f2e2bcae819c686eef4a2d5171d8
github username <github email address>
2020-07-26 12:19:42 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,26 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    count = 1
+    result = []
+    while(count < limit)
+        low = count%3
+        high = count%5
+        if(low == 0)
+            result.push(count)
+        elsif(high == 0)
+            result.push(count)
+        end
+        count += 1
+    end
+    return result
+end
+def sum_multiples(limit)
+    place = collect_multiples(limit)
+    i = 0
+    result = 0
+    while(place[i])
+        result = result + place[i]
+        i = i+1
+    end
+    return result
+end
